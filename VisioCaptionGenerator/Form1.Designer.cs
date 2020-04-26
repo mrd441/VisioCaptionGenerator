3982d1840e1ab663e8b63cac493069f5e5ea671d
mrd441 <441-05@mail.ru>
2020-04-26 21:40:01 +0300

    tmp
    temporary result

--- VisioCaptionGenerator/Form1.Designer.cs
+++ VisioCaptionGenerator/Form1.Designer.cs
@@ -29,12 +29,142 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.logBox = new System.Windows.Forms.RichTextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.fileListElementBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.fileNameDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.makeDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.makeDateDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.checkDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.checkDateDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.tpTypeDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewComboBoxColumn();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.fileListElementBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // logBox
+            // 
+            this.logBox.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.logBox.Location = new System.Drawing.Point(0, 354);
+            this.logBox.Name = "logBox";
+            this.logBox.Size = new System.Drawing.Size(805, 96);
+            this.logBox.TabIndex = 0;
+            this.logBox.Text = "";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.groupBox1.Location = new System.Drawing.Point(0, 0);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(195, 354);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "groupBox1";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoGenerateColumns = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.fileNameDataGridViewTextBoxColumn,
+            this.makeDataGridViewTextBoxColumn,
+            this.makeDateDataGridViewTextBoxColumn,
+            this.checkDataGridViewTextBoxColumn,
+            this.checkDateDataGridViewTextBoxColumn,
+            this.tpTypeDataGridViewTextBoxColumn});
+            this.dataGridView1.DataSource = this.fileListElementBindingSource;
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.dataGridView1.Location = new System.Drawing.Point(195, 0);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(354, 354);
+            this.dataGridView1.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(7, 32);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // fileListElementBindingSource
+            // 
+            this.fileListElementBindingSource.DataSource = typeof(VisioCaptionGenerator.Form1.fileListElement);
+            // 
+            // fileNameDataGridViewTextBoxColumn
+            // 
+            this.fileNameDataGridViewTextBoxColumn.DataPropertyName = "fileName";
+            this.fileNameDataGridViewTextBoxColumn.HeaderText = "fileName";
+            this.fileNameDataGridViewTextBoxColumn.Name = "fileNameDataGridViewTextBoxColumn";
+            // 
+            // makeDataGridViewTextBoxColumn
+            // 
+            this.makeDataGridViewTextBoxColumn.DataPropertyName = "make";
+            this.makeDataGridViewTextBoxColumn.HeaderText = "make";
+            this.makeDataGridViewTextBoxColumn.Name = "makeDataGridViewTextBoxColumn";
+            // 
+            // makeDateDataGridViewTextBoxColumn
+            // 
+            this.makeDateDataGridViewTextBoxColumn.DataPropertyName = "makeDate";
+            this.makeDateDataGridViewTextBoxColumn.HeaderText = "makeDate";
+            this.makeDateDataGridViewTextBoxColumn.Name = "makeDateDataGridViewTextBoxColumn";
+            // 
+            // checkDataGridViewTextBoxColumn
+            // 
+            this.checkDataGridViewTextBoxColumn.DataPropertyName = "check";
+            this.checkDataGridViewTextBoxColumn.HeaderText = "check";
+            this.checkDataGridViewTextBoxColumn.Name = "checkDataGridViewTextBoxColumn";
+            // 
+            // checkDateDataGridViewTextBoxColumn
+            // 
+            this.checkDateDataGridViewTextBoxColumn.DataPropertyName = "checkDate";
+            this.checkDateDataGridViewTextBoxColumn.HeaderText = "checkDate";
+            this.checkDateDataGridViewTextBoxColumn.Name = "checkDateDataGridViewTextBoxColumn";
+            // 
+            // tpTypeDataGridViewTextBoxColumn
+            // 
+            this.tpTypeDataGridViewTextBoxColumn.DataPropertyName = "tpType";
+            this.tpTypeDataGridViewTextBoxColumn.HeaderText = "tpType";
+            this.tpTypeDataGridViewTextBoxColumn.Name = "tpTypeDataGridViewTextBoxColumn";
+            this.tpTypeDataGridViewTextBoxColumn.Resizable = System.Windows.Forms.DataGridViewTriState.True;
+            this.tpTypeDataGridViewTextBoxColumn.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.Automatic;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(805, 450);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.logBox);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.fileListElementBindingSource)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.RichTextBox logBox;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn fileNameDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn makeDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn makeDateDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn checkDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn checkDateDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewComboBoxColumn tpTypeDataGridViewTextBoxColumn;
+        private System.Windows.Forms.BindingSource fileListElementBindingSource;
     }
 }
 
--- VisioCaptionGenerator/Form1.cs
+++ VisioCaptionGenerator/Form1.cs
@@ -8,13 +8,134 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+using IVisio = Microsoft.Office.Interop.Visio;
+using System.IO;
+
 namespace VisioCaptionGenerator
 {
     public partial class Form1 : Form
     {
+        public struct fileListElement
+        {
+            public string fileName { get; set; }
+            public string make { get; set; }
+            public string makeDate { get; set; }
+            public string check { get; set; }
+            public string checkDate { get; set; }
+            public string tpType { get; set; }
+        }
+
+        List<fileListElement> fileList;
+
         public Form1()
         {
             InitializeComponent();
+
+            this.AllowDrop = true;
+            this.DragEnter += new DragEventHandler(Form1_DragEnter);
+            this.DragDrop += new DragEventHandler(Form1_DragDrop);
+
+            fileList = new List<fileListElement>();
+            fileListElementBindingSource.DataSource = fileList;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string templateFileName = Directory.GetCurrentDirectory() + "\\template.vss";
+                string newFullfileName = Directory.GetCurrentDirectory() + "\\template1.vss";
+                if (!File.Exists(templateFileName))                               
+                    throw new Exception("Не найден шаблон выходного файла: " + templateFileName);
+
+                //string fileName = resultList.Last().fileName;
+                //string fullFileName = listBox1.Items[resulFileNumber].ToString();
+                //string fileExt = fileName.Split('.').Last();
+                //string newFileName = fileName.Replace("_schema", "");
+                //string newFilePath = fullFileName.Replace(fileName, "result\\");
+                //string newFullfileName = newFilePath + newFileName;
+
+                IVisio.ApplicationClass visapp = new IVisio.ApplicationClass();
+                IVisio.Document doc = visapp.Documents.Open(templateFileName);
+                IVisio.Page page = doc.Pages[1];
+                IVisio.Master visioRectMaster = doc.Masters.get_ItemU("Sheet.1");
+                
+                IVisio.Shape visioRectShape = page.Drop(visioRectMaster, 4.25, 5.5);
+                visioRectShape.Text = @"Rectangle text.";
+
+                doc.SaveAs(newFullfileName);
+                doc.Close();
+                visapp.Quit();
+
+            }
+            catch(Exception ex)
+            {
+                LogTextEvent(Color.Red, ex.Message);
+            }
+                        
+        }
+
+        public void LogTextEvent( Color TextColor, string EventText)
+        {
+            RichTextBox TextEventLog = logBox;
+            if (TextEventLog.InvokeRequired)
+            {
+                TextEventLog.BeginInvoke(new Action(delegate {
+                    LogTextEvent(TextColor, EventText);
+                }));
+                return;
+            }
+
+            string nDateTime = DateTime.Now.ToString("hh:mm:ss tt") + " - ";
+
+            // color text.
+            TextEventLog.SelectionStart = TextEventLog.Text.Length;
+            TextEventLog.SelectionColor = TextColor;
+
+            // newline if first line, append if else.
+            if (TextEventLog.Lines.Length == 0)
+            {
+                TextEventLog.AppendText(nDateTime + EventText);
+                TextEventLog.ScrollToCaret();
+                TextEventLog.AppendText(System.Environment.NewLine);
+            }
+            else
+            {
+                TextEventLog.AppendText(nDateTime + EventText + System.Environment.NewLine);
+                TextEventLog.ScrollToCaret();
+            }
+        }
+
+        void Form1_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop)) e.Effect = DragDropEffects.Copy;
+        }
+
+        void Form1_DragDrop(object sender, DragEventArgs e)
+        {
+            string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
+            foreach (string file in files)
+            {
+                if (file.Contains(".kml"))
+                {
+                    string fileName = file;
+                    int slashPos = file.LastIndexOf('\\');
+                    if (slashPos != -1)
+                        fileName = file.Substring(slashPos + 1, file.Length - slashPos - 1);
+                    fileListElement fle = new fileListElement();
+                    fle.fileName = fileName;
+                    if (!fileList.Contains(fle))
+                    {
+                        fileList.Add(fle);
+                        LogTextEvent(Color.Black, "Добавлен файл " + file);
+                    }
+                    else
+                        LogTextEvent(Color.Red, "Файл с таким название уже добавлен: " + file);
+                }
+                else
+                    LogTextEvent(Color.Red, "Расширение файла должно быть KLM: " + file);
+            }
+            fileListElementBindingSource.ResetBindings(true);
         }
     }
 }
